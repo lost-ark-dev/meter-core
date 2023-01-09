81592bf5df623f1012d5e95cb1e1633c3f3f61a2
Herysia <totogirerd@gmail.com>
2023-01-09 12:34:40 +0100

    naming for PKTParalyzationStateNotify

--- src/packets/generated/definitions/PKTParalyzationStateNotify.ts
+++ src/packets/generated/definitions/PKTParalyzationStateNotify.ts
@@ -1,12 +1,25 @@
 // Auto Generated, do not edit.
 import { Read } from "../../stream";
 export type PKTParalyzationStateNotify = {
-  Unk0: Buffer;
+  NoHitCheckTime: number;
+  ParalyzationMaxPoint: number;
+  DecreasePoint: number;
+  Enable: boolean;
+  ParalyzationPoint: number;
+  HitCheckTime: number;
+  ObjectId: bigint;
 };
 export function read(buf: Buffer) {
   const reader = new Read(buf);
   const data = {} as PKTParalyzationStateNotify;
-  data.Unk0 = reader.bytes(30);
+  data.NoHitCheckTime = reader.u32();
+  reader.skip(1);
+  data.ParalyzationMaxPoint = reader.u32();
+  data.DecreasePoint = reader.u32();
+  data.Enable = reader.bool();
+  data.ParalyzationPoint = reader.u32();
+  data.HitCheckTime = reader.u32();
+  data.ObjectId = reader.u64();
   return data;
 }
 export const name = "PKTParalyzationStateNotify";

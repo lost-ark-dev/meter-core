321062ef0c012b43e876a2a545f237d579ae4fef
Herysia <totogirerd@gmail.com>
2023-04-12 18:46:31 +0200

    fix TroopMemberUpdateMinNotify

--- src/packets/generated/definitions/PKTTroopMemberUpdateMinNotify.ts
+++ src/packets/generated/definitions/PKTTroopMemberUpdateMinNotify.ts
@@ -3,22 +3,22 @@ import { Read } from "../../stream";
 import * as ReadNBytesInt64 from "../../common/ReadNBytesInt64";
 import * as StatusEffectData from "../structures/StatusEffectData";
 export type PKTTroopMemberUpdateMinNotify = {
-  Unk0: bigint;
-  Unk1: bigint;
+  MaxHp: bigint;
+  CharacterId: bigint;
   Unk0_m: number;
   statusEffectDatas: StatusEffectData.StatusEffectData[];
-  Unk4: bigint;
-  Unk5: bigint;
+  Position: bigint;
+  CurHp: bigint;
 };
 export function read(buf: Buffer) {
   const reader = new Read(buf);
   const data = {} as PKTTroopMemberUpdateMinNotify;
-  data.Unk0 = ReadNBytesInt64.read(reader);
-  data.Unk1 = reader.u64();
+  data.MaxHp = ReadNBytesInt64.read(reader);
+  data.CharacterId = reader.u64();
   data.Unk0_m = reader.u32();
   data.statusEffectDatas = reader.array(reader.u16(), () => StatusEffectData.read(reader), 80);
-  data.Unk4 = reader.u64();
-  data.Unk5 = ReadNBytesInt64.read(reader);
+  data.Position = reader.u64();
+  data.CurHp = ReadNBytesInt64.read(reader);
   return data;
 }
 export const name = "PKTTroopMemberUpdateMinNotify";

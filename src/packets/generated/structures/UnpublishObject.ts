47e48570f9f7565a936d392c0bbc741acbc98838
Herysia <totogirerd@gmail.com>
2024-02-11 19:25:28 +0100

    fix UnpublishObject

--- src/packets/generated/structures/UnpublishObject.ts
+++ src/packets/generated/structures/UnpublishObject.ts
@@ -1,12 +1,12 @@
 // Auto Generated, do not edit.
 import type { Read } from "../../stream";
 export type UnpublishObject = {
-  unpublishReason: number;
   objectId: bigint;
+  unpublishReason: number;
 };
 export function read(reader: Read) {
   const data = {} as UnpublishObject;
-  data.unpublishReason = reader.u8();
   data.objectId = reader.u64();
+  data.unpublishReason = reader.u8();
   return data;
 }
